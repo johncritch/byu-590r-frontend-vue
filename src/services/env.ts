05ada095887ec1709ab21a3e003afb869078482d
John Critchlow <80559126+johncritch@users.noreply.github.com>
2025-04-11 13:18:37 -0600

    fixed url

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,2 +1,5 @@
-const API_URL = "http://127.0.0.1:8000/api/"
+let API_URL = "http://3.14.109.52:8888/api/"
+if (import.meta.env.MODE === "development") {
+	API_URL = "http://127.0.0.1:8000/api/"
+}
 export default API_URL
